827ee38ec30f26e67cd54eb3c0cf171fb574430b
Ignas Butautas <ignasbutautas7@gmail.com>
2020-07-18 22:36:12 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -30,6 +30,7 @@ matrix[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
-matrix[row][column] << new_value
+#matrix[row][column] << new_value
+matrix.insert[row][column]
 
 end
